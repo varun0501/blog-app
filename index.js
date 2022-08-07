6db066ae4680cd2ec4bb9f402af46197b6609f60
Varun Agarwal <vagarwal@vagarwa-ltmrtqk.internal.salesforce.com>
2022-08-06 21:10:25 -0400

    Adding ejs

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = new express();
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const ejs = require("ejs");
+
+// Use ejs templating engine
+app.set("view engine", "ejs");
 
 // Middlewares
+// Serve static pages from public directory
 app.use(express.static("public"));
 
 //Routes
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  //res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 
 app.listen(PORT, () => {
